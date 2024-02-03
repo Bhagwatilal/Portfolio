494a46c214d7ceabf41b04b8081d4a705a881a54
Bhagwatilal <bhagavtilaljoshi1@gmail.com>
2024-02-03 15:33:45 +0530

    updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings: ["Full Stack Developer" , "SPPU Student" , "Web Developer"],
+    strings: ["Full Stack Developer" , "Programmer" , "Web Developer"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
