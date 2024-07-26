436d5861d91f97563f399841aca5e80a67dcbeca
Megan.Ai <megan.ai007>
2024-07-26 16:41:20 +0530

    initial update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings: ["Full Stack Developer" , "Programmer" , "Web Developer"],
+    strings: ["Full Stack Developer" , "Programmer" , "Web Developer" , "Android Developer"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
